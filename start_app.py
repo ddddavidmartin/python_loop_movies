d8d68d20215fde89cd1c6d123e7a0a390999f3f3
David Martin <david_martin@fastmail.com>
2019-11-29 23:37:09 +1100

    Remove unreachable code.
    Running the pycocoa app doesn't return which is why the whole subprocess
    approach is necessary in the first place.

--- start_app.py
+++ start_app.py
@@ -12,8 +12,7 @@ def run_video(video, set_time):
     """Run the given video file in a new thread."""
     app = AppVLC(video=video, set_time=set_time)
     logger.info("Starting the video from the run script")
-    app.run(timeout=None)
-    logger.info("Time at the end: %s", app.player.get_time())
+    app.run(timeout=None)  # never returns
 
 
 def main():
