0c50bd65ae2057120e05b7f47266d4940527e002
David Martin <david_martin@fastmail.com>
2019-11-30 00:10:46 +1100

    Add directory and loop time options.
    The directory is not used yet, but we already use the loop time to
    determine how long each movie will be shown for before switching to the
    next one.

--- loop.py
+++ loop.py
@@ -1,5 +1,6 @@
 """Play videos in a slide show manner."""
 
+import argparse
 import logging
 import subprocess
 import sys
@@ -13,12 +14,17 @@ logger = logging.getLogger(__name__)
 def main():
     """Entry point for our simple vlc player
     """
+    parser = argparse.ArgumentParser(description="Loop through movies in a directory")
+    parser.add_argument('directory', type=str, help="The directory containing the movie files.")
+    parser.add_argument('--loop_time', type=int, default=180,
+                        help="Time in seconds that each movie is shown for at a time.")
+    options = parser.parse_args()
 
-    args = ["python3", "start_app.py", "movie.mp4", "--set_time", "600000"]
+    args = ["python3", "start_app.py", "movies/movie.mp4", "--set_time", "600000"]
     process = subprocess.Popen(args, stdout=sys.stdout, stderr=subprocess.STDOUT)
 
-    logger.info("Letting it run for a bit")
-    time.sleep(5)
+    logger.info("Letting it run for %s second(s)", options.loop_time)
+    time.sleep(options.loop_time)
     process.terminate()
     logger.info("Done")
 
