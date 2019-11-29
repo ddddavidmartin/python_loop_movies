d1518bf7813553d9c4ab73f3bdad12e23d448718
David Martin <david_martin@fastmail.com>
2019-11-30 01:17:03 +1100

    Remove debug output from start_app.py.
    It spams the output and is not really necessary.

--- start_app.py
+++ start_app.py
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 def run_video(video, set_time):
     """Run the given video file in a new thread."""
     app = AppVLC(video=video, set_time=set_time)
-    logger.info("Starting the video from the run script")
     app.run(timeout=None)  # never returns
 
 
